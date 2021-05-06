ca6d42a12780e64f4110b29288ff57c81b9e963c
Maxim Kuvaev <maks-kuvaev@mail.ru>
2021-05-06 15:34:39 +0300

    Внесены первые правки

--- script/script.js
+++ script/script.js
@@ -1,56 +1,47 @@
-let editButton = document.querySelector('.profile__rectangle');
+let editButton = document.querySelector('.profile__edit-button');
 let profName = document.querySelector('.profile__name');
 let description = document.querySelector('.profile__description');
 let addButton = document.querySelector('.profile__add-button');
 let elContainer = document.querySelector('.elements');
 
-let firstPopup = document.querySelector('.popup'); //первый попап
-let closeButtonF = document.querySelector('.popup__close');
+let firstPopup = document.querySelector('.popup_type_edit-form'); //первый попап
+let closeButtonF = document.querySelector('.popup__close_type_edit');
 
-let secondPopup = document.querySelector('.sec-popup'); //второй попап
-let closeButtonS = document.querySelector('.sec-popup__close');
+let secondPopup = document.querySelector('.popup_type_add-card'); //второй попап
+let closeButtonS = document.querySelector('.popup__close_type_add');
 
-let imagePopup = document.querySelector('.image-popup'); //попап картинки
-let closeButtonT = document.querySelector('.image-popup__close');
+let imagePopup = document.querySelector('.popup_type_image'); //попап картинки
+let closeButtonT = document.querySelector('.popup__close_type_image');
 
-let formElement = document.querySelector('.popup__form'); //форма попап 1
+let formElement = document.querySelector('.popup__form_type_edit'); //форма попап 1
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 
-let secondForm = document.querySelector('.sec-popup__form'); //форма попап 2
-let placeInput = secondForm.querySelector('.sec-popup__input_type_place');
-let linkInput = secondForm.querySelector('.sec-popup__input_type_link');
+let secondForm = document.querySelector('.popup__form_type_add'); //форма попап 2
+let placeInput = secondForm.querySelector('.popup__input_type_place');
+let linkInput = secondForm.querySelector('.popup__input_type_link');
 
-function showPopup(type) {
-  if(type === 1) {
-    nameInput.value = profName.textContent;
-    jobInput.value = description.textContent;
-    firstPopup.classList.add('popup_opened');
-  } else {
-    secondPopup.classList.add('sec-popup_opened');
-  }
+function fillForm () {
+  nameInput.value = profName.textContent;
+  jobInput.value = description.textContent;
 }
 
-function closePopup(type) {
-  if(type === 1) {
-    firstPopup.classList.remove('popup_opened');
-  } else if (type === 2)  {
-    secondPopup.classList.remove('sec-popup_opened');
-  } else {
-    imagePopup.classList.remove('image-popup_opened');
-  }
+function openPopup(popup) {
+  fillForm();
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
+    profName.textContent = nameInput.value;
+    description.textContent = jobInput.value;
 
-    profName.textContent = nameValue;
-    description.textContent = jobValue;
-
-    closePopup(1);
+    closePopup(firstPopup);
 }
 
 
@@ -81,62 +72,15 @@ const initialCards = [
   }
 ];
 
-function add () {
-
-  for (let i=0 ; i < initialCards.length ; i++) {
-
-    const elTemplate = document.querySelector('#el-template').content;
-    const element = elTemplate.querySelector('.elements__element').cloneNode(true);
-
-    element.querySelector('.elements__img').src = initialCards[i].link;
-    element.querySelector('.elements__img').alt = initialCards[i].name;
-    element.querySelector('.elements__name').textContent = initialCards[i].name;
-
-    //кнопка лайка
-    element.querySelector('.elements__like').addEventListener('click', function(evt) {
-      evt.target.classList.toggle('elements__like_active');
-    });
-
-    //удаление
-    element.querySelector('.elements__delete').addEventListener('click', function(evt) {
-      //console.log(evt);
-      evt.target.offsetParent.remove();
-    });
-
-    //попап
-    element.querySelector('.elements__overlay').addEventListener('click', function(evt) {
-
-      let imageLink = imagePopup.querySelector('.image-popup__img');
-      let imageText = imagePopup.querySelector('.image-popup__text');
-
-      imagePopup.classList.add('image-popup_opened');
-
-      imageLink.src = evt.path[1].children[0].src;
-      imageLink.alt = evt.path[1].children[0].alt;
-      imageText.textContent = evt.path[1].children[2].children[0].textContent;
-
-
-      //console.log(evt);
-      //evt.path[1].children[2].children[0].textContent;
-    });
-
-    elContainer.append(element);
-  }
-
-}
-
-add();
-
-function secondFormSubmit (evt) {
-  evt.preventDefault();
-
+function createCard(name, link) {
   const elTemplate = document.querySelector('#el-template').content;
-  const element = elTemplate.querySelector('.elements__element').cloneNode(true);
+  const element = elTemplate.querySelector('.elements__card').cloneNode(true);
 
-  element.querySelector('.elements__img').src = linkInput.value;
-  element.querySelector('.elements__img').alt = placeInput.value;
-  element.querySelector('.elements__name').textContent = placeInput.value;
+  let elImage = element.querySelector('.elements__img');
 
+  elImage.src = link;
+  elImage.alt = name;
+  element.querySelector('.elements__name').textContent = name;
 
   //кнопка лайка
   element.querySelector('.elements__like').addEventListener('click', function(evt) {
@@ -145,33 +89,44 @@ function secondFormSubmit (evt) {
 
   //удаление
   element.querySelector('.elements__delete').addEventListener('click', function(evt) {
-    evt.target.offsetParent.remove();
+    evt.target.closest('div').remove();
   });
 
   //попап
   element.querySelector('.elements__overlay').addEventListener('click', function(evt) {
+    let imageLink = imagePopup.querySelector('.popup__img');
+    let imageText = imagePopup.querySelector('.popup__text');
 
-    let imageLink = imagePopup.querySelector('.image-popup__img');
-    let imageText = imagePopup.querySelector('.image-popup__text');
-
-    imagePopup.classList.add('image-popup_opened');
+    imagePopup.classList.add('popup_opened');
 
     imageLink.src = evt.path[1].children[0].src;
     imageLink.alt = evt.path[1].children[0].alt;
     imageText.textContent = evt.path[1].children[2].children[0].textContent;
-
   });
 
-  elContainer.prepend(element);
+  return element;
+}
 
-  closePopup(2);
+initialCards.forEach((card) => {
+  const newCard = createCard(card.name, card.link);
+  elContainer.append(newCard);
+});
+
+
+function secondFormSubmit (evt) {
+  evt.preventDefault();
+
+  const newCard = createCard(placeInput.value, linkInput.value);
+  elContainer.prepend(newCard);
+
+  closePopup(secondPopup);
 }
 
 
-editButton.addEventListener('click', function () {showPopup(1);});
-addButton.addEventListener('click', function () {showPopup(2);});
-closeButtonF.addEventListener('click', function () {closePopup(1)});
-closeButtonS.addEventListener('click', function () {closePopup(2)});
-closeButtonT.addEventListener('click', function () {closePopup(3)});
+editButton.addEventListener('click', function () {openPopup(firstPopup);});
+addButton.addEventListener('click', function () {openPopup(secondPopup);});
+closeButtonF.addEventListener('click', function () {closePopup(firstPopup)});
+closeButtonS.addEventListener('click', function () {closePopup(secondPopup)});
+closeButtonT.addEventListener('click', function () {closePopup(imagePopup)});
 formElement.addEventListener('submit', formSubmitHandler);
 secondForm.addEventListener('submit', secondFormSubmit);
